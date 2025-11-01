f28eea2c4d9b3437e22cb5b9bf75bbd7870df7b7
José Ageu Layme Neto <228136346+joseageu-dev@users.noreply.github.com>
2025-10-31 23:05:31 -0300

    Desafio xadrez nível mestre

--- xadrez.c
+++ xadrez.c
@@ -4,45 +4,58 @@
 // Nível Aventureiro
 // Aluno: José Ageu Layme Neto.
 
+// procedimento de movimento da torre
+void moverTorre(int casas){  
+    if (casas > 0){
+        printf("Direita\n"); // Imprime a direção do movimento
+        moverTorre(casas - 1); // chamada de recursividade e decrementa a quantidade de casas
+     }
+}
+
+// procedimento de movimento do bispo
+void moverBispo(int casas){
+    if (casas > 0){
+        printf("Cima Direita\n"); // Imprime a direção do movimento
+        moverBispo(casas -1); // chamada de recursividade e decrementa a quantidade de casas
+    }    
+}
+
+// procedimento de movimento da rainha
+void moverRainha(int casas){
+    if (casas > 0){
+        printf("Esquerda\n"); // Imprime a direção do movimento
+        moverRainha(casas -1); // chamada de recursividade e decrementa a quantidade de casas
+    }    
+}
+
 int main(){
 
-    int b = 0; //contador do repedidor do movimento do bispo
-    int r = 0; //contador do repedidor do movimento da rainha
-    int movimentoCompleto = 1; //flag para controlar o movimento em L do cavalo
-
     // mover a Torre 5 casas para a direita
+    printf("\n");
     printf("Movimento da Torre:\n");
-    for (int t = 0; t < 5; t++){
-        printf("Direita\n"); // Imprime a direção do movimento
-    }
-
+    moverTorre(5); // chama procediemnto. Parâmetro: quantidade de casas. 
+    
     // Mover o Bispo 5 casas na diagonal para cima e à direita
     printf("\n");
     printf("Movimento do Bispo:\n");
-    while (b < 5){
-        printf("Cima Direita\n"); // Imprime a direção do movimento
-        b++; // incrementa o contador do movimento do bispo
-    }
+    moverBispo(5); // chama procediemnto. Parâmetro: quantidade de casas. 
 
     // Mover a Rainha oito casas para a esquerda
     printf("\n");
     printf("Movimento da Rainha:\n");
-    do{
-        printf("Esquerda\n");
-        r++;
-    } while (r < 8);
+    moverRainha(8); // chama procediemnto. Parâmetro: quantidade de casas. 
 
-    // Mover o Cavalo duas casas para baixo e uma casa para a esquerda
+    // Mover o Cavalo para cima e para a direita, em "L" (duas casas para cima e uma para a direita).
     printf("\n");
     printf("Movimento do Cavalo:\n");
-    while (movimentoCompleto--){
-        for (int c = 0; c < 2; c++){
-            printf("Baixo\n");
+    for (int ch=1; ch < 7; ch++){
+        if (ch > 1){break;}
+        for (int cv = 1; cv < 7; cv++){
+            if (cv > 2){continue;}
+            printf("Cima\n");
         }
-        printf("Esquerda\n");
+        printf("Direita\n");
     }
-    
-
-    
+       
     return 0;
 }
